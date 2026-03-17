11738d59b353b6ec6e3318415cf5def906bafd12
net0008 <herdogmus@hotmail.com>
2026-03-17 10:52:54 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-slate-900 border-t border-slate-800 p-6 mt-auto text-center text-sm text-slate-500">
+      © {new Date().getFullYear()} Bergama CBS - Hasbi Erdoğmuş
+    </footer>
+  );
+}
