779ad2a635b7c3988ff3518140c42da78e3c4a82
net0008 <herdogmus@hotmail.com>
2026-03-17 11:17:31 +0300

    Delete src/components/Footer.tsx

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-slate-900 border-t border-slate-800 p-6 mt-auto text-center text-sm text-slate-500">
-      © {new Date().getFullYear()} Bergama CBS - Hasbi Erdoğmuş
-    </footer>
-  );
-}
